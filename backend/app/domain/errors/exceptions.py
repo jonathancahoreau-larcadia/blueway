0172b473bdf75093cecd2b164d081ee76881b0b7
jonathancahoreau-larcadia <jonathan.cahoreau@gmail.com>
2026-09-20 21:30:23 +0200

    feat(user): add user domain model and errors

--- backend/app/domain/errors/exceptions.py
+++ backend/app/domain/errors/exceptions.py
@@ -4,3 +4,35 @@ class DomainError(Exception):
 
 class ReportNotFoundError(DomainError):
     pass
+
+
+class InvalidFirebaseUidError(DomainError):
+    pass
+
+
+class InvalidUsernameError(DomainError):
+    pass
+
+
+class InvalidEmailError(DomainError):
+    pass
+
+
+class InvalidNationalityError(DomainError):
+    pass
+
+
+class UserAlreadyExistsError(DomainError):
+    pass
+
+
+class UsernameAlreadyExistsError(DomainError):
+    pass
+
+
+class UserNotFoundError(DomainError):
+    pass
+
+
+class EmailNotVerifiedError(DomainError):
+    pass
